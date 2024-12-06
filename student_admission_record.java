d534fc2b121ef5c14e885d09cf7a655bdf3da4ed
Joe Ho <159551341+Sangho12@users.noreply.github.com>
2024-12-06 01:39:17 +0000

    Add files via upload

---
+++ student_admission_record.java
@@ -0,0 +1,13 @@
+import java.io.File;
+
+public class student_admission_record {
+    public String Name;
+    public double Age;
+    public String Gender;
+    public double Score;
+    public double Percentage;
+    public String City;
+    public boolean Isaccepted;
+
+    
+}
